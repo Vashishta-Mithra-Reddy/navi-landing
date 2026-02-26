c4c13c11f38a28f4e1ca7c85b261aed6bf3f87d8
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2026-02-27 04:04:23 +0530

    layout fix

--- components/blocks/header.tsx
+++ components/blocks/header.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion"
 export default function Header() {
   return (
     <header className="w-full border-0 border-t-0">
-      <div className="flex items-center justify-between max-w-4xl w-full mx-auto pt-5 pb-6 px-8 md:px-8 border border-t-0 border-dashed">
+      <div className="flex items-center justify-between max-w-4xl w-full mx-auto pt-3 md:pt-5 pb-4 md:pb-6 px-6 md:px-8 border border-t-0 border-dashed">
         <img src="/logo.png" alt="navi_logo" width={40} height={40} className="w-8 md:w-10 h-8 md:h-10 bg-white rounded-lg"/>
         <motion.p 
           layoutId="navi-logo-text"
